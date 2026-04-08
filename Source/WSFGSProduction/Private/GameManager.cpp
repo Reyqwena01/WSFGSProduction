c619b8187a8cc31df284f954ca941edd41b95eee
Reyqwena01 <157133308+Reyqwena01@users.noreply.github.com>
2026-04-08 18:17:54 +0200

    Update(GameManager)

--- Source/WSFGSProduction/Private/GameManager.cpp
+++ Source/WSFGSProduction/Private/GameManager.cpp
@@ -1,17 +1,17 @@
 #include "GameManager.h"
 // L'inclusion de "InputAction.h" n'est pas nécessaire ici si tu ne l'utilises pas encore.
 
-void UGameManager::SetDifficulty(int difficulty)
-{
-    _difficultyLevel = difficulty; // On assigne la valeur à la variable du .h
-}
-
-void UGameManager::CompareCurrentObject(UUserWidget* Card)
-{
-    if (Card != nullptr)
-    {
-        // Exemple : On ajoute la carte à la liste actuelle
-        _currentQuestRequest.Add(Card);
-    }
-}
+//void UGameManager::SetDifficulty(int difficulty)
+//{
+//    _difficultyLevel = difficulty; // On assigne la valeur à la variable du .h
+//}
+//
+//void UGameManager::CompareCurrentObject(UUserWidget* Card)
+//{
+//    if (Card != nullptr)
+//    {
+//        // Exemple : On ajoute la carte à la liste actuelle
+//        _currentQuestRequest.Add(Card);
+//    }
+//}
 
--- Source/WSFGSProduction/Public/GameManager.h
+++ Source/WSFGSProduction/Public/GameManager.h
@@ -5,7 +5,9 @@
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "Blueprint/UserWidget.h"
+#include "Engine/Texture2D.h"
 #include "GameManager.generated.h"
+
 
 UENUM(BlueprintType)
 enum class EActions : uint8
@@ -24,6 +26,39 @@ enum class EMaterial : uint8
 	Metal,
 	NoneAction
 };
+UENUM(BlueprintType)
+enum class EDirections : uint8
+{
+	TOP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	NoneAction
+};
+
+USTRUCT(BlueprintType)
+struct FSequence
+{
+
+	GENERATED_BODY()
+
+public :
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString _name;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UTexture2D* _image = nullptr; 
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	EDirections _input1 = EDirections::NoneAction;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	EDirections _input2 = EDirections::NoneAction;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	EDirections _input3 = EDirections::NoneAction;
+};
+
 
 /**
  * 
@@ -38,17 +73,27 @@ public:
 	EActions _actions = EActions::NoneAction;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Enumerator")
 	EMaterial _material = EMaterial::NoneAction;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Enumerator")
+	EDirections _currentDirections = EDirections::NoneAction;
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Difficulty")
 	int _difficultyLevel = 0;
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Object")
 	TArray<UUserWidget*> _questRequest;
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Object")
 	TArray<UUserWidget*> _currentQuestRequest;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	TArray<FSequence> _tools;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	TArray<FSequence> _elements;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FSequence _currentUsedTool;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FSequence _currentUsedElement;
 
-	UFUNCTION(BlueprintCallable, Category = "Methode")
+	/*UFUNCTION(BlueprintCallable, Category = "Methode")
 	void SetDifficulty(int difficulty);
 	UFUNCTION(BlueprintCallable, Category = "Methode")
-	void CompareCurrentObject(UUserWidget* Card);
+	void CompareCurrentObject(UUserWidget* Card);*/
 
 };
 
